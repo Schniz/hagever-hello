c52a2ef35976fcacd63a3c6b41e7b132332ca6e5
Gal Schlezinger <gal@spitfire.co.il>
2015-01-24 11:27:08 +0200

    Added gal ve kfir. plus, added procfile which is awesome since i'm gonna use heroku. i think.

---
+++ index.js
@@ -0,0 +1,14 @@
+var port = process.env.PORT || 5000;
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+app.get("/", function(req, res) {
+	res.send("<h1>Gal Hagever. ve gam Kfir.</h1>");
+});
+
+server.listen(port, function() {
+	var address = server.address();
+	console.log("listening on " + address.address + ":" + address.port);
+});
